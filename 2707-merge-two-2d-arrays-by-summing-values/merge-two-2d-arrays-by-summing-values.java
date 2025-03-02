1767e20d61e6124ebc4d0e664fc30d6f7324216a
ADITYA AGARWAL <35536914+adityadev-cmd@users.noreply.github.com>
2025-03-02 18:59:48 +0530

    Time: 1 ms (86.98%) | Memory: 45.9 MB (8.23%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,26 +1,41 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
         int n = nums1.length;
         int m = nums2.length;
-        for (int i = 0 ;i <n;i++){
-            int id = nums1[i][0];
-            int val = nums1[i][1];
-            mp.put(id,mp.getOrDefault(id,0) + val);
-        }
-        for (int i = 0 ;i <m;i++){
-            int id = nums2[i][0];
-            int val = nums2[i][1];
-            mp.put(id,mp.getOrDefault(id,0) + val);
-        }
-        List<int[]> list = new ArrayList<>();
-        for (Map.Entry<Integer,Integer>entry: mp.entrySet()){
-            list.add(new int[]{entry.getKey(),entry.getValue()});
+        List<int[]> res = new ArrayList<>();
+        int i =0 ,j =0;
+        while(i < n && j < m){
+            int id1 = nums1[i][0];
+            int id2 = nums2[j][0];
+            if (id1 == id2){
+                res.add(new int[]{id1,nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if (id1 < id2){
+                res.add(new int[] {id1,nums1[i][1]});
+                i++;
+
+            }
+            else {
+                res.add(new int[] {id2,nums2[j][1]});
+                j++;
+            }
         }
 
-        list.sort(Comparator.comparingInt(a->a[0]));
-
-        return list.toArray(new int[list.size()][]);
-    
+        while(i < n){
+            res.add(new int[] {nums1[i][0],nums1[i][1]});
+            i ++;
+        }
+        while( j < m){
+            res.add(new int[] {nums2[j][0],nums2[j][1]});
+            j++;
+        }
+        int [][] result = new int[res.size()][2];
+        for (int k = 0;k < res.size();k++){
+            result[k] = res.get(k);
+        } 
+        return result;
+        
     }
 }
