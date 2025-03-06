2491980c296a58c03b80b6bcc342c597aea276ff
ADITYA AGARWAL <35536914+adityadev-cmd@users.noreply.github.com>
2025-03-06 21:53:58 +0530

    Time: 628 ms (5.02%) | Memory: 61.1 MB (9.31%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,46 @@
+class Pair implements Comparable<Pair> {
+    int first;
+    int second;
+
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public int compareTo(Pair other) {
+        return Integer.compare(other.second, this.second);
+    }
+}
+
+class Solution {
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        List<Pair> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Pair(speed[i], position[i]));
+        }
+        Collections.sort(list); // on basis of position
+        for (int i = 0; i < n; i++) {
+            System.out.println(list.get(i).first+" "+list.get(i).second);
+        }
+        int prev = -1;
+        List<Double> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int speed1 = list.get(i).first;
+            int position1 = list.get(i).second;
+            Double hr = (target - position1) / (double)speed1;
+
+            if (ans.size() == 0 || ans.get(ans.size() - 1) < hr) {
+                ans.add(hr);
+            }
+        }
+
+        for (Double ele: ans){
+            System.out.println(ele);
+        }
+        
+        return ans.size();
+    }
+}
