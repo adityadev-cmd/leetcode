36206f6134383bdefe00be8dd2a3fdab7194601a
ADITYA AGARWAL <35536914+adityadev-cmd@users.noreply.github.com>
2025-03-25 01:00:26 +0530

    Time: 41 ms (40.40%) | Memory: 100.4 MB (55.56%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        int n = meetings.length;
+        // sort on basis of starting time
+        //merge overlapping intervals
+        Arrays.sort(meetings, Comparator.comparingInt(a->a[0]));
+        int count = 0;
+        // int prevStart = 0;
+        int prevEnd = 0;
+        for (int i = 0; i<n;i++){
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+            if (prevEnd < start){
+                count += start - prevEnd - 1;
+                prevEnd = end;
+            }
+            else {
+                prevEnd = Math.max(prevEnd,end);
+            }
+        }
+
+        if (days > prevEnd){
+            return  days - prevEnd + count;
+        }
+        return count;        
+    }
+}
