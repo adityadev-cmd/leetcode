e4056dc3db3fafee4c26ec0d05faf524a4c33441
ADITYA AGARWAL <35536914+adityadev-cmd@users.noreply.github.com>
2025-05-21 23:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+        int n= matrix.size();
         int m = matrix[0].size();
-        set<int>row;
-        set<int>col;
-        for (int i = 0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+
+        set<int> row;
+        set<int> col;
+
+        for (int i =0 ;i < n; i++){
+            for (int j =0 ;j <m ;j++){
+                if (matrix[i][j] == 0){
+                row.insert(i);
+                col.insert(j);
                 }
             }
         }
 
-        for (auto x : row){
-            for (int j = 0 ;j < m ;j++)
-                matrix[x][j] = 0;
+        for(auto x : row){
+            for (int j =0 ; j< m; j++){
+                matrix[x][j] =0 ;
+            }
         }
 
-        for (auto y : col){
-            for (int i = 0 ;i < n ;i++)
-                matrix[i][y] = 0;
-        }
+     
+            for (int i =0 ; i< n; i++){
+                for(auto y : col){
+                matrix[i][y] =0 ;
+            }
+            }
     }
 };
